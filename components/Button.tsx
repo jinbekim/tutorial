9d3d54a70745bb3f6b54680ff9cbac7e81f6522b
김진범 <jinbekim@ex-em.com>
2025-10-15 20:19:05 +0900

    feat: add primary theme to Button component and update usage in Index screen

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,10 +1,29 @@
+import { FontAwesome } from '@expo/vector-icons';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 type Props = {
   label: string;
+  theme?: 'primary';
 };
 
-export default function Button({ label }: Props) {
+export default function Button({ label, theme }: Props) {
+  if (theme === 'primary') {
+    return (
+        <View
+          style={[
+            styles.buttonContainer,
+            styles.primaryContainer,
+          ]}>
+          <Pressable
+            style={[styles.button, styles.buttonPrimary]}
+            onPress={() => alert('You pressed a button.')}>
+            <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
+            <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
+          </Pressable>
+        </View>
+      );
+    }
+
   return (
     <View style={styles.buttonContainer}>
       {/* Pressable is a component that allows you to press a button single taps or long presses */}
@@ -31,6 +50,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'row',
+  },
+  primaryContainer: {
+    borderWidth: 4,
+    borderColor: '#ffd33d',
+    borderRadius: 18,
+  },
+  buttonPrimary: {
+    backgroundColor: '#fff',
+  },
+  buttonIcon: {
+    paddingRight: 8,
   },
   buttonLabel: {
     color: '#fff',
