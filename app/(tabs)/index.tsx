079caf5b14557e1a7790cad54086f9e4027df2d9
김진범 <jinbekim@ex-em.com>
2025-10-15 20:43:12 +0900

    feat: integrate expo-image-picker for image selection in Index screen and update Button and ImageViewer components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,5 @@
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import Button from '@/components/Button';
@@ -6,13 +8,30 @@ import ImageViewer from '@/components/ImageViewer';
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
+const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+    if (!result.canceled) {
+      // ios가 아니면 heic 파일은 대부분 지원하지 않음
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
+        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button label="Choose a photo" theme="primary" />
+        <Button label="Choose a photo" theme="primary" onPress={pickImageAsync} />
         <Button label="Use this photo" />
       </View>
     </View>
